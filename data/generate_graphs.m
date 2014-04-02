b6708ad74cdb5750328af3aec010a22455ef1aa2
Alexander James Wallar <aw204@st-andrews.ac.uk>
2014-04-02 23:30:40 +0100

    Umm

--- data/generate_graphs.m
+++ data/generate_graphs.m
@@ -4,10 +4,19 @@ con_str = 'data/client_';
 txt = '.txt';
 con_avgs = zeros(1, 20);
 prod_avgs = zeros(1, 20);
+prod_req = zeros(1, 20);
+con_req = zeros(1, 20);
 for i = 1:20
-    con_avgs(i) = mean(load(strcat(con_str, num2str(i * 10), txt))(1:10));
-    prod_avgs(i) = mean(load(strcat(prod_str, num2str(i * 10), txt))(1:10));
+    con_data = load(strcat(con_str, num2str(i * 10), txt));
+    prod_data = load(strcat(prod_str, num2str(i * 10), txt));
+    con_avgs(i) = mean(con_data);
+    prod_avgs(i) = mean(prod_data);
+    prod_req(i) = size(prod_data)(1) / (i);
+    con_req(i) = size(con_data)(1) / (i);
 endfor
+
+prod_req(find(prod_req > 100)) = 100
+con_req(find(con_req > 100)) = 100
 
 figure;
 hold on;
@@ -18,6 +27,19 @@ xlabel('Number of producers / consumers')
 ylabel('Time (seconds)')
 legend('Consumer', 'Producer')
 print('figs/clpr.png', '-dpng');
+
+hold off;
+figure;
+plot(10:10:200, con_req, 'b+-', 'linewidth', 3)
+hold on;
+plot(10:10:200, prod_req, 'r+-', 'linewidth', 3)
+title('Request reliability')
+xlabel('Number of producers / consumers')
+ylabel('% of data transfer')
+legend('Consumer', 'Producer')
+ylim([75 120])
+print('figs/reqs.png', '-dpng');
+
 
 waitforbuttonpress
 
